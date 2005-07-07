b27e0945f0858393eb82838211873d9168f79363
David McClosky <dmcc@bigasterisk.com>
2005-07-07 15:06:47 -0700

    Beginnings of TkEventMaker
    darcs-hash:20050707220647-78b18-11057b4001a433a0099ffd65162d93993c730589.gz

---
+++ src/TkEventMaker.py
@@ -0,0 +1,74 @@
+import Tix as Tk
+from dispatch import dispatcher
+from TkTime import TkTimeWithContext
+from Utility import make_attributes_from_args
+
+bin_names = ("DateTime", "Date", "Time")
+
+class Bin(Tk.Frame):
+    def __init__(self, master, name, contents):
+        Tk.Frame.__init__(self, master)
+        make_attributes_from_args('name', 'contents')
+
+        self.button = Tk.Button(self, text=name)
+        self.button.pack(side='left')
+        self.combo = Tk.ComboBox(self)
+        self.combo.pack(side='left')
+        self.pack(side='left')
+
+        self.listbox = self.combo.slistbox.listbox
+        self.strings_to_parse = {}
+    def draw(self):
+        self.listbox.delete(0, 'end')
+        for score, parse in self.contents:
+            s = str(parse)
+            self.combo.add_history(s)
+            self.strings_to_parse[s] = parse
+
+        if self.contents:
+            self.pack(side='left')
+            self.combo.pick(0)
+        else:
+            self.pack_forget()
+    def select(self, selection):
+        # not used yet
+        print "selected", self.strings_to_parse.get(selection)
+
+class TkEventMaker(TkTimeWithContext):
+    def __init__(self, master):
+        TkTimeWithContext.__init__(self, master)
+        dispatcher.connect(self.reset_bins, "new parses", sender=self.tktime)
+        dispatcher.connect(self.new_segment_parses, "new segment parses", 
+            sender=self.tktime)
+        dispatcher.connect(self.draw_bins, "no parses", sender=self.tktime)
+
+        self.bins = {} # name : Bin object
+        self.bins_sorted = []
+        for name in bin_names:
+            b = Bin(self, name=name, contents=[])
+            self.bins[name] = b
+            self.bins_sorted.append(b)
+    def reset_bins(self):
+        for bin in self.bins.values():
+            bin.contents = []
+    def new_segment_parses(self, segment, parses):
+        for parse, score in parses:
+            for bin_name in bin_names:
+                bin = self.bins[bin_name]
+                method = getattr(parse, "as_%s" % bin_name.lower())
+                result = method()
+                if result:
+                    bin.contents.append((score, result))
+                    break
+        self.draw_bins()
+    def draw_bins(self):
+        for bin in self.bins_sorted:
+            bin.contents.sort()
+            bin.draw()
+
+if __name__ == "__main__":
+    root = Tk.Tk()
+    root.title("The Event Maketron")
+    tktimewithcontext = TkEventMaker(root)
+    tktimewithcontext.pack(fill='both', expand=1)
+    Tk.mainloop()
